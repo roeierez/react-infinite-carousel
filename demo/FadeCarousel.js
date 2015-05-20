ab87e14ae96ea687f9cd55c3f25362ad0a8a3d69
Roei Erez <roeie@harmon.ie>
2015-05-20 14:13:58 +0300

    some experinments

--- demo/FadeCarousel.js
+++ demo/FadeCarousel.js
@@ -36,7 +36,8 @@ var content = document.getElementById('content'),
     },
 
     backgroundRenderer = function (index) {
-        return <div style={{height: '100%', width: '100%', backgroundColor: 'red'}}></div>;
+        //return <div style={{height: '100%', width: '100%', backgroundColor: 'red'}}></div>;
+        return <img style={{width: '100%', height: '100%'}} src={images[index % 3]} />;
         //return <img style={{filter: 'brightness(30%)', WebkitFilter: 'brightness(40%)', width: '100%', height: '100%'}} src={images[index % 3]} />;
     }
 
