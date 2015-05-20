6afc0934340c38ec75961f38efa014cc56668db8
Roei Erez <roeie@harmon.ie>
2015-05-20 14:10:58 +0300

    some experinments

--- demo/FadeCarousel.js
+++ demo/FadeCarousel.js
@@ -36,7 +36,7 @@ var content = document.getElementById('content'),
     },
 
     backgroundRenderer = function (index) {
-        return <div></div>;
+        return <div style={{height: '100%', width: '100%', backgroundColor: 'red'}}></div>;
         //return <img style={{filter: 'brightness(30%)', WebkitFilter: 'brightness(40%)', width: '100%', height: '100%'}} src={images[index % 3]} />;
     }
 
--- demo/build/FadeCarousel.js
+++ demo/build/FadeCarousel.js
@@ -36,7 +36,7 @@ var content = document.getElementById('content'),
     },
 
     backgroundRenderer = function (index) {
-        return React.createElement("div", null);
+        return React.createElement("div", {style: {height: '100%', width: '100%', backgroundColor: 'red'}});
         //return <img style={{filter: 'brightness(30%)', WebkitFilter: 'brightness(40%)', width: '100%', height: '100%'}} src={images[index % 3]} />;
     }
 
